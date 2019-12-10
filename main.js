bff0623e836c8400130932defcfab46549bf72d3
Evan Daniel <Evan.Daniel@sckans.edu>
2019-12-09 18:43:39 -0600

    implement setnextquestion() and selectanswer()

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const startButton =document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
-
+let currentQuestionNumber = -1; 
 
 startButton.addEventListener('click', startGame)
 
@@ -12,10 +12,17 @@ function startGame(){
 
 }
 function setNextQuestion(){
-
+    currentQuestionNumber++; 
+    document.querySelector('#question').innerText = questions[currentQuestionNumber].question; 
+    document.querySelectorAll('#question-container button').forEach( (button, index) => {
+        button.innerText = questions[currentQuestionNumber].answer[index].text; 
+        button.addEventListener('click', event => selectAnswer(questions[currentQuestionNumber].answer[index].correct)); 
+    });
 }
-function selectAnswer(){
-
+function selectAnswer(isCorrect){
+    console.log(isCorrect); 
+    document.querySelector('.feedback').innerHTML += `<div class="feedback">That answer was ${isCorrect}</div>`; 
+    if(isCorrect) setNextQuestion(); 
 }
 const questions = [
     {
@@ -25,7 +32,15 @@ const questions = [
             {text:'Franklin', correct: false},
             {text: 'Obama', correct: false},
             {text: 'Trump', correct: false}
-
         ]
-    }
+    }, 
+    {
+        question: 'Who invented the World Wide Web?', 
+        answer: [
+            {text: 'Johann Sebastian Bach', correct: false},
+            {text: 'Ludwig van Beethoven', correct: false},
+            {text: 'Tim Berners-Lee', correct: true},
+            {text: 'Claude Debussy', correct: false},
+        ]
+    }, 
 ]
